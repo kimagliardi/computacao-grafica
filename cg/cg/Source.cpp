8426c3f181afcc43f1225e06a0c58f2408419208
kimagliardi <kim.agliardi@gmail.com>
2018-03-13 21:29:26 -0300

    Dois triangulos
    Desenho do triangulo pronto..

--- cg/cg/Source.cpp
+++ cg/cg/Source.cpp
@@ -128,6 +128,37 @@ int main()
 		}
 		if (doubleT == true) {
 			vShader.use();
+
+			float firstTriangle[] = {
+				-0.9f, -0.5f, 0.0f,  // left 
+				-0.0f, -0.5f, 0.0f,  // right
+				-0.45f, 0.5f, 0.0f,  // top 
+			};
+			float secondTriangle[] = {
+				0.0f, -0.5f, 0.0f,  // left
+				0.9f, -0.5f, 0.0f,  // right
+				0.45f, 0.5f, 0.0f   // top 
+			};
+
+			glGenVertexArrays(2, VAOs); // we can also generate multiple VAOs or buffers at the same time
+			glGenBuffers(2, VBOs);
+			// first triangle setup
+			// --------------------
+			glBindVertexArray(VAOs[0]);
+			glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);	// Vertex attributes stay the same
+			glEnableVertexAttribArray(0);
+			// glBindVertexArray(0); // no need to unbind at all as we directly bind a different VAO the next few lines
+			// second triangle setup
+			// ---------------------
+			glBindVertexArray(VAOs[1]);	// note that we bind to a different VAO now
+			glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);	// and a different VBO
+			glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle, GL_STATIC_DRAW);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0); // because the vertex data is tightly packed we can also specify 0 as the vertex attribute's stride to let OpenGL figure it out
+			glEnableVertexAttribArray(0);
+			// glBindVertexArray(0); // not really necessary as well, but beware of calls that could affect VAOs while this one is bound (like binding element buffer objects, or enabling/disabling vertex attributes)
+
 			glBindVertexArray(VAOs[0]);
 			glDrawArrays(GL_TRIANGLES, 0, 3);
 			// then we draw the second triangle using the data from the second VAO
@@ -138,6 +169,7 @@ int main()
 
 		}
 
+		
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
 	}
@@ -165,35 +197,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	}
 	
 	if (key == GLFW_KEY_X && action == GLFW_PRESS) {
-		float firstTriangle[] = {
-			-0.9f, -0.5f, 0.0f,  // left 
-			-0.0f, -0.5f, 0.0f,  // right
-			-0.45f, 0.5f, 0.0f,  // top 
-		};
-		float secondTriangle[] = {
-			0.0f, -0.5f, 0.0f,  // left
-			0.9f, -0.5f, 0.0f,  // right
-			0.45f, 0.5f, 0.0f   // top 
-		};
-
-		glGenVertexArrays(2, VAOs); // we can also generate multiple VAOs or buffers at the same time
-		glGenBuffers(2, VBOs);
-		// first triangle setup
-		// --------------------
-		glBindVertexArray(VAOs[0]);
-		glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);	// Vertex attributes stay the same
-		glEnableVertexAttribArray(0);
-		// glBindVertexArray(0); // no need to unbind at all as we directly bind a different VAO the next few lines
-		// second triangle setup
-		// ---------------------
-		glBindVertexArray(VAOs[1]);	// note that we bind to a different VAO now
-		glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);	// and a different VBO
-		glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle, GL_STATIC_DRAW);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0); // because the vertex data is tightly packed we can also specify 0 as the vertex attribute's stride to let OpenGL figure it out
-		glEnableVertexAttribArray(0);
-		// glBindVertexArray(0); // not really necessary as well, but beware of calls that could affect VAOs while this one is bound (like binding element buffer objects, or enabling/disabling vertex attributes)
+		doubleT = !doubleT;
 	}
 
 }
